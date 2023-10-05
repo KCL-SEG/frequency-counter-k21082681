d6c467663125ad72769db733a4f333b40c9cca21
k21082681 <shahad.alhamazani@kcl.ac.uk>
2023-10-05 19:48:49 +0100

    Assignment solved

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,28 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    if len(items) == 0:
+        return frequencies
+    
+    count = 0
+    prev_item = items[0]
+
+    for i in  range(0,len(items)):
+        items[i] = str(items[i])
+    items.sort()
+
+    for item in items:
+        if item == prev_item:
+            count += 1
+        else:
+            prev_item = item
+            count = 1
+        frequencies[item] = count
+        
     return frequencies
+
+dict = frequencies([100, 'Hello', '100', '100', 100, "Haha"])
+print(dict.items())
+
+
+
